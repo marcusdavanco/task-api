78fbcf639230015894a00ea9b9948b9b02261099
Marcus Davanço <md.daves@gmail.com>
2023-07-13 11:41:56 -0300

    feat: Routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
 import http from 'node:http'
 
 const server = http.createServer((req, res) => {
-	return res.end('Hello World')
+	const { method, url } = req
+
+	if (method === 'GET' && url === '/tasks'){
+		return res.end('Listagem de tarefas\n');
+	}
+
+	if (method === 'POST' && url === '/tasks'){
+		return res.end('Criação de tarefa\n');
+	}
+
 })
 
 server.listen(3333)
