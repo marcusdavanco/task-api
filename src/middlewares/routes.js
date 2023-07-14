6af6883b542c5c2e852b5efea318caef81171c84
Marcus Davanço <md.daves@gmail.com>
2023-07-14 18:11:34 -0300

    feat: removal of registers

--- src/middlewares/routes.js
+++ src/middlewares/routes.js
@@ -39,7 +39,13 @@ export const routes = [
     method: 'DELETE',
     path: buildRoutePath('/tasks/:id'),
     handler: (req, res) => {
-      return res.end()
+      const { id } = req.params
+
+      database.delete('tasks', id)
+      
+      return res
+        .writeHead(204)
+        .end()
     },
   }
 ]
